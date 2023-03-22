97f745bd8ae8b661d40275ae3e29db1563fafd18
Dolbozavr <124203240+Dolbozavr@users.noreply.github.com>
2023-03-22 10:26:23 +0300

    Add Wires
    Добавлены провода с крутой физикой

---
+++ Assets/Multimetr/Scripts/WireBlack.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireBlack : MonoBehaviour
+{
+    private Vector3 basepos;
+    private Vector3 needpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        basepos = gameObject.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
